43c83379e4699d6c2f7654a057ca136389897d57
arshad-qadri <arshadqadri321@gmail.com>
2021-04-30 18:06:35 +0530

    changes

--- js/main.js
+++ js/main.js
@@ -3,20 +3,128 @@ const output = document.getElementById("output");
 const grams = document.getElementById("grams");
 const tonne = document.getElementById("tonne");
 const pound = document.getElementById("pound");
+const kilogram = document.getElementById("kilogram");
 const miligrams = document.getElementById("miligrams");
 const microgram = document.getElementById("microgram");
+let val = "";
 
-output.style.display = "none";
-weight.addEventListener("input", e => {
-  if (e.target.value === "") {
-    output.style.display = "none";
-  } else {
-    output.style.display = "block";
-    let val = e.target.value;
-    tonne.innerHTML = `Tonne : ${(val * 0.001).toFixed(3)}`;
-    grams.innerHTML = `Grams : ${val * 1000}`;
-    miligrams.innerHTML = `Mili Grams : ${val * 1e6}`;
-    microgram.innerHTML = `Micro Grams : ${val * 1e9}`;
-    pound.innerHTML = `Pound : ${(val * 2.20462).toFixed(3)}`;
+const getslectedValue = () => {
+  const slectedValue = document.getElementById("list").value;
+  pound.style.display = "block";
+  microgram.style.display = "block";
+  miligrams.style.display = "block";
+  grams.style.display = "block";
+  tonne.style.display = "block";
+  kilogram.style.display = "block";
+
+  // ----------------------kilogram--------------------
+  if (slectedValue === "KiloGram") {
+    weight.placeholder = "Enter Kilo Gram";
+    kilogram.style.display = "none";
+
+    if (val === "") {
+      output.style.display = "none";
+    } else {
+      output.style.display = "block";
+
+      tonne.innerHTML = `Tonne : ${(val * 0.001).toFixed(3)}`;
+      grams.innerHTML = `Gram : ${val * 1000}`;
+      miligrams.innerHTML = `Miligram : ${val * 1e6}`;
+      microgram.innerHTML = `Microgram : ${val * 1e9}`;
+      pound.innerHTML = `Pound : ${(val * 2.20462).toFixed(3)}`;
+      console.log(slectedValue);
+    }
   }
+  // -----------tonne-------------------
+  else if (slectedValue === "Tonne") {
+    weight.placeholder = "Enter Tonne";
+    tonne.style.display = "none";
+    if (val === "") {
+      output.style.display = "none";
+    } else {
+      output.style.display = "block";
+      kilogram.innerHTML = `Kilogram : ${(val * 1000).toFixed(3)}`;
+      grams.innerHTML = `Grams : ${val * 1e6}`;
+      miligrams.innerHTML = `Miligram : ${val * 1e9}`;
+      microgram.innerHTML = `Microgram : ${val * 1e12}`;
+      pound.innerHTML = `Pound : ${(val * 2204.62).toFixed(3)}`;
+      console.log(slectedValue);
+    }
+    console.log(slectedValue);
+  }
+  // ---------------------gram-----------------------
+  else if (slectedValue === "Gram") {
+    weight.placeholder = "Enter Gram";
+    grams.style.display = "none";
+    if (val === "") {
+      output.style.display = "none";
+    } else {
+      output.style.display = "block";
+      kilogram.innerHTML = `Kilogram : ${(val * 0.001).toFixed(3)}`;
+      tonne.innerHTML = `Tonne : ${val * 1e-6}`;
+      miligrams.innerHTML = `Miligram : ${val * 1000}`;
+      microgram.innerHTML = `Microgram : ${val * 1e6}`;
+      pound.innerHTML = `Pound : ${(val * 0.00220462).toFixed(3)}`;
+      console.log(slectedValue);
+    }
+    console.log(slectedValue);
+  }
+  // ----------------miligram------------------
+  else if (slectedValue === "MiliGram") {
+    weight.placeholder = "Enter Mili Gram";
+    miligrams.style.display = "none";
+    if (val === "") {
+      output.style.display = "none";
+    } else {
+      output.style.display = "block";
+      kilogram.innerHTML = `Kilogram : ${val * 1e-6}`;
+      tonne.innerHTML = `Tonne : ${val * 1e-9}`;
+      grams.innerHTML = `Grams : ${val * 0.001}`;
+      microgram.innerHTML = `Microgram : ${val * 1000}`;
+      pound.innerHTML = `Pound : ${(val * 2.2046e-6).toFixed(3)}`;
+      console.log(slectedValue);
+    }
+    console.log(slectedValue);
+  }
+  // --------------microgram-------------------
+  else if (slectedValue === "MicroGram") {
+    weight.placeholder = "Enter Micro Gram";
+    microgram.style.display = "none";
+    if (val === "") {
+      output.style.display = "none";
+    } else {
+      output.style.display = "block";
+      kilogram.innerHTML = `Kilogram : ${val * 1e-9}`;
+      tonne.innerHTML = `Tonne : ${val * 1e-12}`;
+      grams.innerHTML = `Gram : ${val * 1e-6}`;
+      miligrams.innerHTML = `Miligram : ${val * 0.001}`;
+      pound.innerHTML = `Pound : ${(val * 2.2046e-9).toFixed(3)}`;
+      console.log(slectedValue);
+    }
+    console.log(slectedValue);
+  }
+  // -------------pound------------------------------
+  else if (slectedValue === "Pound") {
+    pound.style.display = "none";
+    weight.placeholder = "Enter Pound";
+    if (val === "") {
+      output.style.display = "none";
+    } else {
+      output.style.display = "block";
+      kilogram.innerHTML = `Kilogram : ${val * 0.453592}`;
+      tonne.innerHTML = `Tonne : ${val * 0.000453592}`;
+      grams.innerHTML = `Gram : ${val * 453.592}`;
+      miligrams.innerHTML = `Miligram : ${val * 453592}`;
+      microgram.innerHTML = `Microgram : ${(val * 4.536e8).toFixed(3)}`;
+      console.log(slectedValue);
+    }
+    console.log(slectedValue);
+  }
+};
+
+weight.addEventListener("input", e => {
+  val = e.target.value;
+  getslectedValue();
 });
+
+getslectedValue();
